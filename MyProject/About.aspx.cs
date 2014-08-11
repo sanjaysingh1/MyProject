de9e4d4b062a5156512a0929f2ea1b34c0ac2a8a
sanjay.singh1 <sanjay.singh1@kaplan.com>
2014-08-11 15:04:06 +0530

    Test Commit1234 #Release7.1

--- MyProject/About.aspx.cs
+++ MyProject/About.aspx.cs
@@ -11,7 +11,7 @@ namespace MyProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string str = "test 1";
+            string str = "test12345";
         }
     }
 }
